347e65e2984af67cc65fc713d216883372118ee3
MrPink <sead.pavica@chainium.io>
2018-05-23 15:14:11 +0200

    simplify app - used for  jenkins configuration

---
+++ Simplify/Simplify.App/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "This is one simple app."
+    0 // return an integer exit code
---
+++ Simplify/Simplify.IntegrationTests/Tests.fs
@@ -0,0 +1,8 @@
+module Tests
+
+open System
+open Xunit
+
+[<Fact>]
+let ``Simplify - integration test`` () =
+    Assert.True(true)
