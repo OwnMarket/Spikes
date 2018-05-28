12c574336035ecc2037972caf249ce62c07882b8
MrPink <sead.pavica@chainium.io>
2018-05-28 11:06:44 +0200

    updates to simplify test project

--- Simplify/Simplify.IntegrationTests/Tests.fs
+++ Simplify/Simplify.IntegrationTests/Tests.fs
@@ -2,7 +2,12 @@ module Tests
 
 open System
 open Xunit
+open Swensen.Unquote
 
 [<Fact>]
 let ``Simplify - integration test`` () =
     Assert.True(true)
+
+[<Fact>]
+let ``Simplify - additional integration test `` () =
+    test <@ true = true @>
