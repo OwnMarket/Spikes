cb9433b29351f15b39e8d95b67e85c07219abe7a
marian-gheorghe <marian.gheorghe@weown.com>
2019-07-05 17:06:17 +0200

    [PyCrypto] generatateWallet, addressFromPrivateKey, decompress a verifying key

--- PyCrypto/Crypto.py
+++ PyCrypto/Crypto.py
@@ -2,6 +2,7 @@ import base58
 import base64
 import hashlib
 import struct
+from ecdsa import SigningKey, SECP256k1
 
 ####################################################################################################
 ## Encoding
@@ -37,15 +38,36 @@ def hash(data):
 
 def deriveHash(address, nonce, txActionNumber):
     addressB = decode58(address)
-    nonceB = (nonce).to_bytes(8, byteorder='big')
-    txActionNumberB = (txActionNumber).to_bytes(2, byteorder='big')
+    nonceB = nonce.to_bytes(8, byteorder='big')
+    txActionNumberB = txActionNumber.to_bytes(2, byteorder='big')
     return hash(addressB + nonceB + txActionNumberB)
 
 def blockchainAddress(publicKey):
-    prefix = bytearray.fromhex('065A')
+    prefix = bytes(bytearray.fromhex('065A'))
     publicKeyHashWithPrefix = prefix + sha160(sha256(publicKey))
     checksum = sha256(sha256(publicKeyHashWithPrefix))[:4]
     return encode58(publicKeyHashWithPrefix + checksum)
+
+####################################################################################################
+## Signing
+####################################################################################################
+
+def decompress(pk):
+     return bytes(chr(4), 'ascii') + pk.to_string()
+     
+def generateWallet(): 
+    sk = SigningKey.generate(curve=SECP256k1)
+    pk = sk.get_verifying_key()
+    privateKey = encode58(sk.to_string())
+    publicKey = bytes(chr(4), 'ascii') + pk.to_string()
+    address = blockchainAddress(decompress(pk))
+    return (privateKey, address)    
+    
+def addressFromPrivateKey(privateKey):      
+    privateKeyB = decode58(privateKey)
+    sk = SigningKey.from_string(privateKeyB, curve=SECP256k1)
+    pk = sk.get_verifying_key()
+    return blockchainAddress(decompress(pk))
 
 ####################################################################################################
 ## Testing
@@ -81,7 +103,22 @@ def testDeriveHash():
     actual = deriveHash(address, nonce, txActionNumber)
     print('Expected = ', expected, ' | Actual = ', actual)
 
+def testGenerateWallet():
+    privateKey, address = generateWallet()
+    expected = address   
+    actual = addressFromPrivateKey(privateKey)
+    print('Expected = ', expected, ' | Actual = ', actual)    
+    
+def testAddressFromPrivateKey():
+    privateKey = '3rzY3EENhYrWXzUqNnMEbGUr3iEzzSZrjMwJ1CgQpJpq'
+    expected = 'CHGmdQdHfLPcMHtzyDzxAkTAQiRvKJrkYv8'
+    actual = addressFromPrivateKey(privateKey)
+    print('Expected = ', expected, ' | Actual = ', actual)    
+        
 testEncodeDecodeBase64()
 testEncodeDecodeBase58()
 testHash()
 testDeriveHash()
+testGenerateWallet()
+testAddressFromPrivateKey()
+
