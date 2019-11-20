6325207495cd96e55cda86edaf7d040b99f2ecdc
marian-gheorghe <marian.gheorghe@weown.com>
2019-11-20 09:38:44 +0100

    UdpClient request / response

---
+++ UdpDiscovery/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PORT = 9875;
+            for (var i = 1; i <= 1000; i ++ )
+            {
+                var data = Encoding.UTF8.GetBytes(i.ToString());
+                UdpClient udpClient = new UdpClient();
+                udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 9875 + i));
+
+                var from = new IPEndPoint(0, 0);
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        var recvBuffer = udpClient.Receive(ref from);
+                        Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+                    }
+                });
+
+                udpClient.Send(data, data.Length, "127.0.0.1", PORT);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdpDiscovery/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PORT = 9875;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
+
+            var from = new IPEndPoint(0, 0);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var recvBuffer = udpClient.Receive(ref from);
+
+                    Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+
+                    if (recvBuffer.Any())
+                        udpClient.Send(recvBuffer, recvBuffer.Length, "127.0.0.1", 9876);
+                }
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
